77c9722c6050bc73f1d63690717fb4b4206f71b4
MakarovPI-222 <79741417+MakarovPI-222@users.noreply.github.com>
2022-01-04 13:32:54 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
     echo "<p><a href='lab-1-1.php'>Типы переменных</a></p>";
     echo "<p><a href='lab-1-2.php'>Арифметические операции</a></p>"; 
     echo "<p><a href='lab-1-3.php'>Константа</a></p>";
-    
-  
+    echo "<p><a href='lab-1-5.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-1.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-2.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-3.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-4.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-5.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-6.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-7.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-8.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-9.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-10.php'>Типы переменных</a></p>";
+    echo "<p><a href='lab-2-11.php'>Типы переменных</a></p>";
 ?>
