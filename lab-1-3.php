79b8aa222ffe630d935060e11f0962ea2e6a161a
MakarovPI-222 <79741417+MakarovPI-222@users.noreply.github.com>
2022-01-05 12:45:21 +0500

    Add files via upload

--- lab-1-3.php
+++ lab-1-3.php
@@ -2,5 +2,6 @@
  define(NUM_E,2.71828);
  echo('Число е равное' . ' ' . NUM_E . '<br>'); 
  $num_el=(integer) NUM_E;
- echo('num_el' . ' ' . '=' . ' ' . $num_el . ' ' . '-' . gettype($num_el));
+ echo('e=');
+ var_dump(NUM_E);
 ?>
